920f79d61ad77d17f2d8c17736bff2d52043c80a
ALE1CANA <alessandro.salvi07@icloud.com>
2025-05-16 13:57:07 +0200

    Add files via upload

---
+++ Client/Client/Program.cs
@@ -0,0 +1,80 @@
+﻿﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ProtocolTcpClient
+{
+    class Client
+    {
+        public static int Main(string[] args)
+        {
+            StartClient();
+            return 0;
+        }
+
+        public static void StartClient()
+        {
+            byte[] buffer = new byte[1024];
+
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                using (Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    sender.Connect(remoteEP);
+                    Console.WriteLine("Connesso al server!");
+
+                    Console.Write("Vuoi giocare al quiz? (si/no): ");
+                    string playChoice = Console.ReadLine().ToLower();
+
+                    byte[] msg = Encoding.ASCII.GetBytes(playChoice + "<EOF>");
+                    sender.Send(msg);
+
+                    if (playChoice != "si")
+                    {
+                        Console.WriteLine("OK!");
+                        return;
+                    }
+
+                    int numQuestions;
+                    do
+                    {
+                        Console.Write("Quante domande vuoi? (1-5): ");
+                    } while (!int.TryParse(Console.ReadLine(), out numQuestions) || numQuestions < 1 || numQuestions > 5);
+
+                    msg = Encoding.ASCII.GetBytes(numQuestions.ToString() + "<EOF>");
+                    sender.Send(msg);
+
+                    Console.WriteLine("\nIniziamo il quiz!\n");
+
+                    for (int i = 0; i < numQuestions; i++)
+                    {
+                        int bytesRec = sender.Receive(buffer);
+                        string question = Encoding.ASCII.GetString(buffer, 0, bytesRec).Replace("<EOF>", "");
+                        Console.WriteLine($"Domanda {i + 1}: {question}");
+
+                        Console.Write("La tua risposta: ");
+                        string answer = Console.ReadLine();
+                        msg = Encoding.ASCII.GetBytes(answer + "<EOF>");
+                        sender.Send(msg);
+                    }
+
+                    int finalBytesRec = sender.Receive(buffer);
+                    string score = Encoding.ASCII.GetString(buffer, 0, finalBytesRec).Replace("<EOF>", "");
+                    Console.WriteLine($"\n{score}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Errore: {e.Message}");
+            }
+
+            Console.WriteLine("\nPremi un tasto per uscire...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ProtocolloTcp
+{
+    class Server
+    {
+        private static readonly string[] AllQuestions =
+        {
+            "Qual e' la capitale della Spagna?",
+            "Qual e' la prima squadra di Roma?",
+            "Come si chiama il presidente della repubblica (cognome)?",
+            "Chi ha vinto il mondiale nel 2006? ",
+            "Quanto fa 123456789*0?"
+        };
+
+        private static readonly string[] AllAnswers =
+        {
+            "Roma",
+            "Roma",
+            "Mattarella",
+            "Italia",
+            "0"
+        };
+
+        public static int Main(string[] args)
+        {
+            StartServer();
+            return 0;
+        }
+
+        public static void StartServer()
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            try
+            {
+                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                Console.WriteLine("In attesa di connessioni...");
+
+                while (true)
+                {
+                    Socket handler = listener.Accept();
+                    Console.WriteLine("Client connesso!");
+
+                    try
+                    {
+                        byte[] buffer = new byte[1024];
+
+                        int bytesRec = handler.Receive(buffer);
+                        string playChoice = Encoding.ASCII.GetString(buffer, 0, bytesRec).Replace("<EOF>", "");
+
+                        if (playChoice.ToLower() != "si")
+                        {
+                            Console.WriteLine("Client non partecipare al quiz. Chiudo connessione.");
+                            handler.Shutdown(SocketShutdown.Both);
+                            handler.Close();
+                            continue;
+                        }
+
+                        bytesRec = handler.Receive(buffer);
+                        int numQuestions = int.Parse(Encoding.ASCII.GetString(buffer, 0, bytesRec).Replace("<EOF>", ""));
+
+                        if (numQuestions < 1) numQuestions = 1;
+                        if (numQuestions > 5) numQuestions = 5;
+
+                        Console.WriteLine($"Client vuole {numQuestions} domande");
+
+                        int score = 0;
+                        Random rnd = new Random();
+
+                        for (int i = 0; i < numQuestions; i++)
+                        {
+                            int qIndex = rnd.Next(AllQuestions.Length);
+                            string question = AllQuestions[qIndex];
+                            string correctAnswer = AllAnswers[qIndex];
+
+                            string questionMessage = question + "<EOF>";
+                            byte[] msg = Encoding.ASCII.GetBytes(questionMessage);
+                            handler.Send(msg);
+                            Console.WriteLine($"Inviata domanda: {question}");
+
+                            bytesRec = handler.Receive(buffer);
+                            string answer = Encoding.ASCII.GetString(buffer, 0, bytesRec)
+                                .Replace("<EOF>", "").Trim().ToLower();
+                            Console.WriteLine($"Ricevuta risposta: {answer}");
+
+                            if (answer == correctAnswer)
+                            {
+                                score++;
+                            }
+                        }
+
+                        string scoreMessage = $"Il tuo punteggio finale e': {score} su {numQuestions}<EOF>";
+                        byte[] scoreMsg = Encoding.ASCII.GetBytes(scoreMessage);
+                        handler.Send(scoreMsg);
+                    }
+                    finally
+                    {
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Errore: {e.Message}");
+            }
+        }
+    }
+}
